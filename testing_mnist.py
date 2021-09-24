c1a4b2495b2a12e64d35b7d110ddb5c3844881c6
Kevyn Kelso <vyn@COH3034-LAP33.uccs.edu>
2021-09-24 11:36:43 -0600

    fix: update testing_mnist script

--- testing_mnist.py
+++ testing_mnist.py
@@ -1,5 +1,12 @@
 import tensorflow as tf
+import numpy as np
 import glob
+
+from accordion_mnist import get_formatted_mnist_classification_data
+
+from sklearn.metrics import (precision_score,
+                             recall_score,
+                             f1_score)
 
 def get_model_name(file_path):
     file_name = file_path.split('/')[-1]
@@ -18,19 +25,26 @@ def get_model_name(file_path):
 
 def main():
     baseline_mnist_models = glob.glob('./models/mnist_accordion_classification_models/*baseline*.h5')
+    (x_train, y_train), (x_test, y_test) = get_formatted_mnist_classification_data()
 
     for model_file in baseline_mnist_models:
-        print(get_model_name(model_file))
         model = tf.keras.models.load_model(model_file)
         model.summary()
+
+        y_test_pred = model.predict(y_test)
+
+        # parameters for csv need: name,val_loss,val_accuracy,precision,recall,f1,complexity
+        model_name = get_model_name(model_file)
+        trainableParams = np.sum([np.prod(v.get_shape()) for v in model.trainable_weights])
+
+        precision = precision_score(y_test, y_test_pred)
+        recall = recall_score(y_test, y_test_pred)
+        f1 = f1_score(y_test, y_test_pred)
+
+        print('name,val_loss,val_accuracy,precision,recall,f1,complexity')
+        print(f'{model_name},{precision},{recall},{f1},{trainableParams}')
+
         break
-
-    names = [get_model_name(x) for x in baseline_mnist_models]
-    names.sort()
-
-    # load data and test
-    for name in names:
-        print(name)
 
 if __name__ == "__main__":
     main()
