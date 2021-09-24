c003df46ffecca8b79cd000a2f3418472f099cdb
Kevyn Kelso <vyn@COH3034-LAP33.uccs.edu>
2021-09-24 11:14:26 -0600

    fix: update testing_mnist script

--- testing_mnist.py
+++ testing_mnist.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 import glob
 
 def get_model_name(file_path):
@@ -18,8 +19,16 @@ def get_model_name(file_path):
 def main():
     baseline_mnist_models = glob.glob('./models/mnist_accordion_classification_models/*baseline*.h5')
 
-    names = [get_model_name(x) for x in baseline_mnist_models]
+    for model_file in baseline_mnist_models:
+        print(get_model_name(model_file))
+        model = tf.keras.models.load_model(model_file)
+        model.summary()
+        break
 
+    names = [get_model_name(x) for x in baseline_mnist_models]
+    names.sort()
+
+    # load data and test
     for name in names:
         print(name)
 
