fix: remove average=binary from testing mnist

testing_mnist.py
@@ -39,9 +39,9 @@ def main():
         model_name = get_model_name(model_file)
         trainableParams = np.sum([np.prod(v.get_shape()) for v in model.trainable_weights])
 
-        precision = precision_score(y_test, y_test_pred)
-        recall = recall_score(y_test, y_test_pred)
-        f1 = f1_score(y_test, y_test_pred)
+        precision = precision_score(y_test, y_test_pred, average='weighted')
+        recall = recall_score(y_test, y_test_pred, average='weighted')
+        f1 = f1_score(y_test, y_test_pred, average='weighted')
 
         print('name,val_loss,val_accuracy,precision,recall,f1,complexity')
         print(f'{model_name},{precision},{recall},{f1},{trainableParams}')